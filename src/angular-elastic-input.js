91b5915880106e1bb609f08a74eb004c978ea41c
Jacek Pulit <jacek.pulit@gmail.com>
2014-09-18 00:05:44 +0200

    create directive source file

---
+++ src/angular-elastic-input.js
@@ -0,0 +1,21 @@
+/**
+ * Angular Elastic Input Directive
+ * A directive for AngularJS which automatically resizes the width of input field according to the content, while typing.
+ * @author: Jacek Pulit <jacek.pulit@gmail.com>
+ * @license: The MIT License
+ */
+
+'use strict';
+
+angular.module('ngElasticInput', []).directive('elasticInput', function(){
+    return {
+        restrict: 'A',
+        link: function postLink(scope, element, attrs) {
+
+
+
+
+
+        }
+    };
+});
