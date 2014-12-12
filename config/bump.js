5f7f755962e6d5240029cbfcd8c1767c92e92b46
Jacek Pulit <jacek.pulit@gmail.com>
2014-12-13 00:56:25 +0100

    increasing version number during the build task

---
+++ config/bump.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+
+    options: {
+        files: ['package.json', 'bower.json'],
+        updateConfigs: ['pkg'],
+        commit: false,
+        createTag: false,
+        push: false
+    }
+
+};
--- tasks/build.js
+++ tasks/build.js
@@ -1,12 +1,28 @@
 
 module.exports = function (grunt) {
 
-    grunt.registerTask('build', [
+    grunt.registerTask('build', 'Run all build tasks.', function(bump) {
 
-        'clean',
-        'ngmin',
-        'uglify'
+        var tasks = [
+            'clean',
+            'ngmin',
+            'uglify'
+        ];
 
-    ]);
+        if (bump) {
+
+            if(['patch','minor','major'].indexOf(bump) > -1) {
+                tasks.unshift('bump:' + bump);
+            } else {
+                grunt.fail.fatal('Wrong parameter name, please use one of following options: patch, minor or major.');
+            }
+
+        } else {
+            grunt.log.subhead("You can increase version number automatically using parameter :patch, :minor or :major.");
+        }
+
+        grunt.task.run(tasks);
+
+    });
 
 };
