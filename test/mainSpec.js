55f6d325c911cbc68b9215f071f92b6e5706761e
Tomás Fox <tomas.c.fox@gmail.com>
2015-08-23 13:07:43 -0300

    Add more tests

--- test/mainSpec.js
+++ test/mainSpec.js
@@ -33,6 +33,12 @@ describe('puElasticInput', function() {
         ctrl.$render();
     }
 
+    function triggerEvent(element, eventName) {
+        var event = document.createEvent('HTMLEvents');
+        event.initEvent(eventName, true, true);
+        element[0].dispatchEvent(event);
+    }
+
     describe('width', function() {
 
         it('increases when some text is written', function() {
@@ -40,6 +46,50 @@ describe('puElasticInput', function() {
             var initialWidth = element[0].offsetWidth;
             setInputValue(element, 'foobar');
             expect(element[0].offsetWidth).toBeGreaterThan(initialWidth);
+        });
+
+        it('is changed when model is changed in scope', function() {
+            var element = compileAndDigest('<input ng-model="s" pu-elastic-input>');
+            var initialWidth = element[0].offsetWidth;
+            scope.s = 'foobar';
+            scope.$digest();
+            expect(element[0].offsetWidth).toBeGreaterThan(initialWidth);
+        });
+
+        it('is changed when ngModelOptions.updateOn event is triggered', function() {
+            var t = '<input ng-model="s" ng-model-options="{updateOn: \'blur\'}" pu-elastic-input>';
+            var element = compileAndDigest(t);
+            var initialWidth = element[0].offsetWidth;
+            setInputValue(element, 'foobar');
+            expect(element[0].offsetWidth).toBe(initialWidth);
+            triggerEvent(element, 'blur');
+            expect(element[0].offsetWidth).toBeGreaterThan(initialWidth);
+        });
+
+        it('is changed to fit the placeholder', function() {
+            var regularElement =
+                compileAndDigest('<input ng-model="s" pu-elastic-input>');
+            var placeholderElement =
+                compileAndDigest('<input ng-model="s" placeholder="foobar" pu-elastic-input>');
+            expect(placeholderElement[0].offsetWidth).toBeGreaterThan(regularElement[0].offsetWidth);
+        });
+
+        it('cannot be less than minWidth', function() {
+            var style = 'min-width: 100px; padding:0; margin:0; border:none';
+            var element =
+                compileAndDigest('<input ng-model="s" style="' + style + '" pu-elastic-input>');
+            expect(element[0].clientWidth).toBe(100);
+            setInputValue(element, 'foobar_foobar_foobar_foobar_foobar');
+            expect(element[0].clientWidth).toBeGreaterThan(100);
+        });
+
+        it('cannot be more than maxWidth', function() {
+            var style = 'max-width: 100px; padding:0; margin:0; border:none';
+            var element =
+                compileAndDigest('<input ng-model="s" style="' + style + '" pu-elastic-input>');
+            expect(element[0].clientWidth).toBeLessThan(100);
+            setInputValue(element, 'foobar_foobar_foobar_foobar_foobar');
+            expect(element[0].clientWidth).toBe(100);
         });
 
     });
